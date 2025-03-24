0e4686076c300f3bce57d1f05c05d0296235f5dc
SREELAKSHMI S <sreelakshmis112001@gmail.com>
2025-03-24 17:59:27 +0530

    odd number added

--- magic square.py
+++ magic square.py
@@ -2,6 +2,7 @@
 numbers, usually distinct integers, in a square,
 such that the n numbers in all rows, all columns, and both diagonals sum to the same constant. A
 magic square contains the integers from 1 to n^2. Create a magic square of a given order N.'''
+
 import numpy as np
 
 def odd_magical_square(n):
