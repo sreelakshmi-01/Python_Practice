128f4fc7433b7f9e40f9a7a656bd0fc6a04429f3
Sreelakshmi S <sreelakshmis112001@gmail.com>
2025-05-28 22:32:44 +0530

    Update burgerTown.py

--- burgerTown.py
+++ burgerTown.py
@@ -1,6 +1,7 @@
 def max_happiness(N, restaurants):
     # Sort restaurants based on their position (X)
     restaurants.sort()
+    
     # Dictionary to store max happiness at each position
     dp = {}
 
